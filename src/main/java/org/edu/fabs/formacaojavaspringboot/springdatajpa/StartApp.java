dc7e91b42e577a7b22c325b12ca37b98758ef2f5
bianavic <bianavic@gmail.com>
2023-11-01 17:30:36 -0300

    [springframework] spring data jpa

---
+++ src/main/java/org/edu/fabs/formacaojavaspringboot/springdatajpa/StartApp.java
@@ -0,0 +1,36 @@
+package org.edu.fabs.formacaojavaspringboot.springdatajpa;
+
+import org.edu.fabs.formacaojavaspringboot.springdatajpa.model.User;
+import org.edu.fabs.formacaojavaspringboot.springdatajpa.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+@Component
+public class StartApp implements CommandLineRunner {
+
+    @Autowired
+    private UserRepository repository;
+
+    @Override
+    public void run(String... args) throws Exception {
+        List<User> users = repository.findByNameContaining("GLEYSON");
+        for(User u: users){
+            System.out.println(u);
+        }
+    }
+    private void insertUser(){
+        User user = new User();
+        user.setName("NERO CRISTINA");
+        user.setUsername("nero");
+        user.setPassword("123");
+        repository.save(user);
+
+        for(User u: repository.findAll()){
+            System.out.println(u);
+        }
+    }
+
+}
