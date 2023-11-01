5defef5145e9250c996a6a32e1e66f521b1a23bf
bianavic <bianavic@gmail.com>
2023-11-01 16:36:42 -0300

    [springframework] properties value

---
+++ src/main/java/org/edu/fabs/formacaojavaspringboot/propertiesvalue/Main.java
@@ -0,0 +1,11 @@
+package org.edu.fabs.formacaojavaspringboot.propertiesvalue;
+
+import org.springframework.boot.SpringApplication;
+
+public class Main {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Main.class, args);
+    }
+
+}
